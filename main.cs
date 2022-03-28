1e77b74b429b145822ae081600eb1e69d9c53e6f
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-28 16:42:28 +0000

    deleted comments

--- main.cs
+++ main.cs
@@ -12,19 +12,17 @@ class Program
         // This function accepts user input
         int hoursworked;
         int hourlyrate;
-        //input
+
         Console.WriteLine("This program determines your pay and the amount you have to give the government.");
         Console.WriteLine("");
         Console.Write("Enter the amount of hours worked: ");
         hoursworked = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter your hourly rate: ");
         hourlyrate = Convert.ToInt32(Console.ReadLine());
-      
-      //process
-      Double pay = (hoursworked * hourlyrate) * (1.00 - 0.18);
+
+        Double pay = (hoursworked * hourlyrate) * (1.00 - 0.18);
         Double taxes = (hoursworked * hourlyrate) * 0.18;
       
-    //output
         Console.WriteLine("");
         Console.Write("Your pay will be: " + " $" + pay.ToString("0.00"));
         Console.ReadKey();
